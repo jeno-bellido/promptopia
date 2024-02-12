455d5dd3090d7be7f9e5aa19453a5874acb4ba46
Jeno Bellido <jeno.bellido@wvsu.edu.ph>
2024-02-12 20:24:20 +0800

    asda

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -8,7 +8,6 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 
 import Form from '@components/Form';
-import { data } from 'autoprefixer';
 
 const EditPrompt = () => {
     const router = useRouter();
