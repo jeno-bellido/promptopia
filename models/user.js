b63ad76deb44b8fabde2493676a4985bdbf2267f
Jeno Bellido <jeno.bellido@wvsu.edu.ph>
2024-02-13 16:14:05 +0800

    asda

--- models/user.js
+++ models/user.js
@@ -7,10 +7,10 @@ const UserSchema = new Schema({
         required: [true, 'Email is required.']
     },
     username: {
-        type: String,
-        required: [true, 'Username is required.'],
-        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid. it should contain 8-20 alphanumeric letters and be unique."]
-    },
+      type: String,
+      required: [true, 'Username is required.'],
+      match: [/^(?=.{1,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid. It should contain 1-20 alphanumeric letters and be unique."]
+  },
     image: {
         type: String,
     }
